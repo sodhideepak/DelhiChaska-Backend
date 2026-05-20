265a1ad38ce22288a36773b854ed49fb20515e8d
sodhideepak <deepaksodhi0023@gmail.com>
2026-05-21 00:20:50 +0530

    fix: update sameSite attribute to 'none' in cookieOptions for compatibility

--- src/utils/cookieOptions.js
+++ src/utils/cookieOptions.js
@@ -3,6 +3,7 @@
 export const cookieOptions = {
     httpOnly: true,
     secure: true,      // true in production
-    sameSite: "lax",
-    path: "/"
+    sameSite: "none",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 * 1000
 }
