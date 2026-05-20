69e8378b55a1655bcc4fb5c73a839b6679063f81
sodhideepak <deepaksodhi0023@gmail.com>
2026-05-20 19:23:56 +0530

    fix: update deliveryDays to include Monday for next delivery date calculation

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1859,7 +1859,7 @@ const getNextDeliveryDate = asynchandler(async (req, res) => {
     // 🔥 CONFIG (DYNAMIC)
     // ==========================
     // const deliveryDays = [1, 3, 5]; // Monday, Wednesday, Friday
-    const deliveryDays = [3]; // Monday, Wednesday, Friday
+    const deliveryDays = [1]; // Monday, Wednesday, Friday
     const cutoffHour = 22; // 10 PM
 
     // ==========================
