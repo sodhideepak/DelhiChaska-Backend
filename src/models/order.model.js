485d0e0f0a09706e08367359808d66fb39c4eba6
sodhideepak <deepaksodhi0023@gmail.com>
2026-05-26 22:10:39 +0530

    fix: comment out unused imports and function in getNextDeliveryDate utility

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { getNextDeliveryDate } from "../utils/getNextDeliveryDate";
+// import { getNextDeliveryDate } from "../utils/getNextDeliveryDate";
 
 // ─────────────────────────────────────────────
 // ORDER ITEM SCHEMA
--- src/utils/getNextDeliveryDate.js
+++ src/utils/getNextDeliveryDate.js
@@ -1,57 +1,57 @@
 
-import { asynchandler } from "../utils/asynchandler.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { ApiError } from "../utils/ApiError.js";
+// import { asynchandler } from "../utils/asynchandler.js";
+// import { ApiResponse } from "../utils/ApiResponse.js";
+// import { ApiError } from "../utils/ApiError.js";
 
-export const getNextDeliveryDate = (
-  baseDate = new Date()
-) => {
+// export const getNextDeliveryDate = (
+//   baseDate = new Date()
+// ) => {
 
-  // Monday & Thursday
-  const deliveryDays = [1, 4];
+//   // Monday & Thursday
+//   const deliveryDays = [1, 4];
 
-  // Convert to US Pacific Time
-  const usDate = new Date(
-    baseDate.toLocaleString(
-      "en-US",
-      {
-        timeZone:
-          "America/Los_Angeles"
-      }
-    )
-  );
+//   // Convert to US Pacific Time
+//   const usDate = new Date(
+//     baseDate.toLocaleString(
+//       "en-US",
+//       {
+//         timeZone:
+//           "America/Los_Angeles"
+//       }
+//     )
+//   );
 
-  const today =
-    usDate.getDay();
+//   const today =
+//     usDate.getDay();
 
-  let daysToAdd = null;
+//   let daysToAdd = null;
 
-  // Find next delivery day
-  for (
-    let i = 1;
-    i <= 7;
-    i++
-  ) {
+//   // Find next delivery day
+//   for (
+//     let i = 1;
+//     i <= 7;
+//     i++
+//   ) {
 
-    const nextDay =
-      (today + i) % 7;
+//     const nextDay =
+//       (today + i) % 7;
 
-    if (
-      deliveryDays.includes(nextDay)
-    ) {
+//     if (
+//       deliveryDays.includes(nextDay)
+//     ) {
 
-      daysToAdd = i;
-      break;
-    }
-  }
+//       daysToAdd = i;
+//       break;
+//     }
+//   }
 
-  // Create next delivery date
-  const nextDeliveryDate =
-    new Date(usDate);
+//   // Create next delivery date
+//   const nextDeliveryDate =
+//     new Date(usDate);
 
-  nextDeliveryDate.setDate(
-    usDate.getDate() + daysToAdd
-  );
+//   nextDeliveryDate.setDate(
+//     usDate.getDate() + daysToAdd
+//   );
 
-  return nextDeliveryDate;
-};
+//   return nextDeliveryDate;
+// };
